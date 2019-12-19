875bf0a719153eecdd6eb5694e4a1c715142d242
Eugene Tulushev <eugene@tulushev.com>
2019-12-19 18:17:24 +0200

    Add project

---
+++ README.md
@@ -0,0 +1,101 @@
+# Firebase Cloud Functions for HyperTrack Ridesharing sample app
+
+This repository contains Firebase Cloud Functions that create and complete trips and manage orders for Ridesharing apps for [iOS](https://github.com/hypertrack/ridesharing-ios) and [Android](https://github.com/hypertrack/ridesharing-android).
+
+* [Requirements](#requirements)
+* [Add to your project](#add-to-your-project)
+* [Documentation](#documentation)
+* [FAQs](#frequently-asked-questions)
+* [Contribute](#contribute)
+* [Support](#support)
+
+## Requirements
+
+Firebase Cloud Functions require a paid account to make outbound API calls. This includes API calls to HyperTrack APIs. We recommend using the Blaze plan. It gives a lot of bandwidth for free, which is more then enough to internally test the app.
+
+## Add to your project
+
+1. Clone the repo.
+
+```bash
+git clone https://github.com/hypertrack/uber-for-x-firebase.git
+cd uber-for-x-firebase
+```
+
+2. Install Firebase tools CLI
+
+```bash
+npm install -g firebase-tools
+```
+
+3. Login to your account and choose your project
+
+```bash
+firebase login
+firebase use --add
+```
+Select the project from dropdown menu
+
+
+4. Install function dependencies
+
+```bash
+cd functions ; npm install ; cd ..
+```
+
+5. Configure functions with HyperTrack API Keys
+
+Copy `AccountId` and `SecretKey` from HyperTrack's dashboard [setup page](https://dashboard.hypertrack.com/setup). Paste them in `/functions/index.js` file into placeholders on line 9. Keep the `:` separator between them.
+
+6. Deploy functions into your Firebase Ridesharing project
+
+```bash
+firebase deploy --only functions
+```
+
+7. Setup webhooks from HyperTrack to Cloud Functions
+
+* Open the Functions > Dashboard tab in your Firebase project
+* Copy the trigger URL for `onTripUpdate` function
+  ![Functions Dashboard](Images/FunctionsDashboard.png)
+* Go to HyperTrack's dashboard [setup page](https://dashboard.hypertrack.com/setup) and add this URL as a webhook URL.
+* Go back to Firebase and open the Logs tab. Search for `onTripUpdate` function's log with `SubscriptionConfirmation` message. Open the `SubscribeURL` in browser. This will activate the webhook.
+  ![Functions Logs](Images/FunctionsLogs.png)
+
+8. Your Cloud Functions backend is now ready to interface with HyperTrack APIs.
+
+## Documentation
+
+For detailed documentation of the APIs, customizations and what all you can build using HyperTrack, please visit the official [docs](https://docs.hypertrack.com/).
+
+## Frequently Asked Questions
+
+* [No project active](#no-project-active)
+* [Error parsing triggers: Cannot find module 'moment'](#error-parsing-triggers-cannot-find-module-moment)
+
+### No project active
+
+```
+No project active. Run with --project <projectId> or define an alias by
+running firebase use --add
+```
+
+This error while deploying functions means that you need to select the project first. Run `firebase use --add` and select your project from dropdown menu, then add any alias to the project.
+
+### Error parsing triggers: Cannot find module 'moment'
+
+This error can happen when your didn't run `npm install` from the `functions` directory. Run `cd functions ; npm install ; cd ..` from the projects directory or execute the commands one by one:
+
+```bash
+cd functions
+npm install
+cd ..
+```
+
+## Contribute
+Feel free to clone, use, and contribute back via [pull requests](https://help.github.com/articles/about-pull-requests/). We'd love to see your pull requests - send them in! Please use the [issues tracker](https://github.com/hypertrack/ridesharing-firebase/issues) to raise bug reports and feature requests.
+
+We are excited to see what live location feature you build in your app using this project. Do ping us at help@hypertrack.com once you build one, and we would love to feature your app on our blog!
+
+## Support
+Join our [Slack community](https://join.slack.com/t/hypertracksupport/shared_invite/enQtNDA0MDYxMzY1MDMxLTdmNDQ1ZDA1MTQxOTU2NTgwZTNiMzUyZDk0OThlMmJkNmE0ZGI2NGY2ZGRhYjY0Yzc0NTJlZWY2ZmE5ZTA2NjI) for instant responses. You can also email us at help@hypertrack.com.
---
+++ functions/index.js
@@ -0,0 +1,200 @@
+const moment = require('moment');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const request = require('request-promise');
+const Base64 = require('js-base64').Base64;
+
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
+const AUTHORIZATION = "Basic " + Base64.encode('AccountId:SecretKey');
+
+const DATE_FORMAT = "YYYY-MM-DD[T]HH:mm:ss[Z]";
+
+// firebase deploy --only functions
+
+// DEVELOPING:
+
+// export GOOGLE_APPLICATION_CREDENTIALS="path to your json file like uber-for-x-58779-firebase-adminsdk-vkrvn-56c58820cc.json"
+
+// Test https:
+// firebase emulators:start --only functions
+
+// Test firestore:
+// firebase functions:shell
+
+
+// Test functions with valid json data, it means replace data in testData.json with your existing trips and drivers.
+// updateOrderStatus(require('./testData.json').acceptOrder)
+async function acceptOrder(change, context) {
+    const beforeValue = change.before.data();
+    const afterValue = change.after.data();
+
+    const parsedBody = await createTrip(afterValue.driver.device_id, afterValue.pickup);
+
+    if (parsedBody["trip_id"]) {
+        const updatedAt = moment().format(DATE_FORMAT);
+        return change.after.ref.set({
+            updated_at: updatedAt,
+            status: "PICKING_UP",
+            trip_id: parsedBody["trip_id"]
+        }, {merge: true});
+    }
+}
+
+// updateOrderStatus(require('./testData.json').startRide)
+async function startRide(change, context) {
+    const beforeValue = change.before.data();
+    const afterValue = change.after.data();
+
+    await completeTrip(beforeValue.trip_id);
+    const parsedBody = await createTrip(afterValue.driver.device_id, afterValue.dropoff);
+
+    if (parsedBody["trip_id"]) {
+        const updatedAt = moment().format(DATE_FORMAT);
+        return change.after.ref.set({
+            updated_at: updatedAt,
+            status: "DROPPING_OFF",
+            trip_id: parsedBody["trip_id"]
+        }, {merge: true});
+    }
+}
+
+// updateOrderStatus(require('./testData.json').rejectRide)
+async function rejectRide(change, context) {
+    const beforeValue = change.before.data();
+
+    await completeTrip(beforeValue.trip_id);
+
+    const updatedAt = moment().format(DATE_FORMAT);
+    return change.after.ref.set({
+        updated_at: updatedAt,
+        trip_id: null,
+        driver: null,
+    }, {merge: true});
+}
+
+// updateOrderStatus(require('./testData.json').endRide)
+async function endRide(change, context) {
+    const beforeValue = change.before.data();
+
+    await completeTrip(beforeValue.trip_id);
+
+    const updatedAt = moment().format(DATE_FORMAT);
+    return change.after.ref.set({
+        updated_at: updatedAt,
+    }, {merge: true});
+}
+
+async function createTrip(deviceId, coordinates) {
+    try {
+        const data = {};
+        data["device_id"] = deviceId;
+        data["destination"] = {
+            radius: 100,
+            geometry: {
+                "type": "Point",
+                "coordinates": [coordinates.longitude, coordinates.latitude]
+            }
+        };
+
+        const parsedBody = await request({
+            method: 'POST',
+            uri: 'https://v3.api.hypertrack.com/trips/',
+            headers: {
+                'Authorization': AUTHORIZATION
+            },
+            body: data,
+            json: true // Automatically stringifies the body to JSON
+        });
+        console.log("createTrip: " + JSON.stringify(parsedBody));
+        console.log("createTrip:trip_id: " + parsedBody["trip_id"]);
+
+        return parsedBody;
+    } catch (err) {
+        console.error(err.message);
+        console.trace();
+    }
+}
+
+async function completeTrip(tripId) {
+    if (tripId) {
+        try {
+            const parsedBody = await request({
+                method: 'POST',
+                uri: `https://v3.api.hypertrack.com/trips/${tripId}/complete`,
+                headers: {
+                    'Authorization': AUTHORIZATION
+                },
+                body: null
+            });
+            console.log("completeTrip: " + JSON.stringify(parsedBody));
+
+            return parsedBody;
+        } catch (err) {
+            console.error(err.message);
+            console.trace();
+        }
+    }
+}
+
+exports.updateOrderStatus = functions.firestore
+    .document('orders/{orderId}')
+    .onUpdate(async (change, context) => {
+        console.log("before: " + JSON.stringify(change.before.data()));
+        console.log("after: " + JSON.stringify(change.after.data()));
+        const beforeStatus = change.before.data().status;
+        const afterStatus = change.after.data().status;
+
+        if (beforeStatus === "NEW" && afterStatus === "ACCEPTED") {
+            await acceptOrder(change, context);
+        } else if (beforeStatus === "REACHED_PICKUP" && afterStatus === "STARTED_RIDE") {
+            await startRide(change, context);
+        } else if (beforeStatus !== "COMPLETED" && afterStatus === "COMPLETED") {
+            await endRide(change, context);
+        } else if (beforeStatus !== "CANCELLED" && afterStatus === "CANCELLED") {
+            await rejectRide(change, context);
+        }
+    });
+
+// deleteOrder(require('./testData.json').deleteOrder)
+exports.deleteOrder = functions.firestore
+    .document('orders/{orderId}')
+    .onDelete(async (snap, context) => {
+        // Get an object representing the document prior to deletion
+        const deletedValue = snap.data();
+        await completeTrip(deletedValue.trip_id);
+    });
+
+// http://localhost:5001/<your project name e.g. uber-for-x-58779>/us-central1/onTripUpdate
+exports.onTripUpdate = functions.https.onRequest(async (req, res) => {
+    const data = JSON.parse(req.body);
+    console.log("onTripUpdate:data: " + JSON.stringify(data));
+    for (let i = 0; i < data.length; i++) {
+        const event = data[i];
+        console.log("onTripUpdate:event: " + JSON.stringify(event));
+        if (event.type === "trip" && event.data.value === "destination_arrival") {
+            let ordersRef = firestore.collection("orders");
+
+            let querySnapshot = await ordersRef.where("trip_id", "==", event.data.trip_id).limit(1).get();
+            let status, doc_id;
+            querySnapshot.forEach((doc) => {
+                doc_id = doc.id;
+                console.log("onTripUpdate:doc.data: " + doc.id + " - " + JSON.stringify(doc.data()));
+                if (doc.data().status === "PICKING_UP") {
+                    status = "REACHED_PICKUP";
+                } else if (doc.data().status === "DROPPING_OFF") {
+                    status = "REACHED_DROPOFF";
+                }
+            });
+            console.log("onTripUpdate:status: " + status);
+
+            if (status) {
+                await ordersRef.doc(doc_id).update({
+                    status: status
+                });
+            }
+        }
+    }
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.send("I ♥ HyperTrack");
+});
